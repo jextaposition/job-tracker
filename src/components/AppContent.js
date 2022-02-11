6b478e00656050f32dc360794ed988947e0e4c91
Alexa Hayes <alexa.joy.hayes@gmail.com>
2022-02-10 20:14:35 -0800

    fixed filter to show complete, or incomplete items

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -6,11 +6,20 @@ import styles from '../styles/modules/app.module.scss';
 const AppContent = () => {
   const jobList = useSelector((state) => state.jobs.jobList);
   const sortedJobList = [...jobList];
+  const filterStatus = useSelector((state) => state.jobs.filterStatus);
   sortedJobList.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+  const filteredJobList = sortedJobList.filter((item) => {
+    if (filterStatus === 'all') {
+      return true;
+    }
+    return item.status === filterStatus;
+  });
+
   return (
     <div className={styles.content__wrapper}>
-      {sortedJobList && sortedJobList.length > 0
-        ? sortedJobList.map((job) => <JobItem key={job.id} job={job} />)
+      {filteredJobList && filteredJobList.length > 0
+        ? filteredJobList.map((job) => <JobItem key={job.id} job={job} />)
         : 'no jobs found'}
     </div>
   );
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import JobModal from './JobModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateFilterStatus } from '../slices/jobSlice';
 
 const AppHeader = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const filterStatus = useSelector((state) => state.jobs.filterStatus);
+  const dispatch = useDispatch();
 
+  const updateFilter = (e) => {
+    dispatch(updateFilterStatus(e.target.value));
+  };
   return (
     <div className={styles.appHeader}>
       <Button variant='primary' onClick={() => setModalOpen(true)}>
         Add New Job
       </Button>
-      <SelectButton id='status'>
+      <SelectButton id='status' value={filterStatus} onChange={updateFilter}>
         <option value='all'>ALL</option>
         <option value='incomplete'>Incomplete</option>
         <option value='complete'>Complete</option>
