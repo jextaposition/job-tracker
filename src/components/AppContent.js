74fa41327f3d9eebad2a9c0e62cce348476826ed
Alexa Hayes <alexa.joy.hayes@gmail.com>
2022-02-10 19:43:01 -0800

    fixed check to complete or incomplete job

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import JobItem from './JobItem';
+import styles from '../styles/modules/app.module.scss';
 
 const AppContent = () => {
   const jobList = useSelector((state) => state.jobs.jobList);
   const sortedJobList = [...jobList];
   sortedJobList.sort((a, b) => new Date(b.time) - new Date(a.time));
   return (
-    <div>
+    <div className={styles.content__wrapper}>
       {sortedJobList && sortedJobList.length > 0
         ? sortedJobList.map((job) => <JobItem key={job.id} job={job} />)
         : 'no jobs found'}
